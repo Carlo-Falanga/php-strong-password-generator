7d6f664f37087ee43e77e723426d7c3039744708
Carlo-Falanga <carlofalanga7@gmail.com>
2026-06-10 10:16:47 +0200

    Move password generation logic to functions.php and include it in index.php

---
+++ functions.php
@@ -0,0 +1,21 @@
+<?php
+
+function generatePassword($length)
+{
+    $characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*";
+    $password = "";
+
+    for ($i = 0; $i < $length; $i++) {
+        $password .= $characters[rand(0, strlen($characters) - 1)];
+    }
+
+    return $password;
+}
+
+$password = "";
+
+if (isset($_GET["length"])) {
+    $password = generatePassword($_GET["length"]);
+}
+
+?>
