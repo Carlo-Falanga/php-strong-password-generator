81fa1646551280a0aa5e42f3f04416a7208712b9
Carlo-Falanga <carlofalanga7@gmail.com>
2026-06-10 10:06:09 +0200

    Add password generation function to index.php

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php
 
+function generatePassword($length){
+    $characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*";
+    $password = "";
 
+    for ($i = 0; $i < $length; $i++){
+        $password .= $characters[rand(0, strlen($characters) -1)];
+    }
+
+    return $password;
+}
 
 ?>
 
